a801f50b109c8ae7d4b195ed09821758efa5c9a8
Rizky Ageng Nugroho <102507383+RizkyNugroho666@users.noreply.github.com>
2022-06-18 11:07:34 +0700

    Update V-Sync_Disabler.py

--- Source Code/V-Sync_Disabler.py
+++ Source Code/V-Sync_Disabler.py
@@ -16,7 +16,7 @@ gamePlatfrom = ["WindowsNoEditor", "EGS", "WinGDK"] # [Steam] [Epic Games] [Micr
 def menu(): # Menu
     ascii()
     print("Select What You Want!?")
-    print("[1] Disable V-Sync Steam\n[2] Disable V-Sync Epic Games\n[3] Disable V-Sync Microsoft Store\n[4] Fix Config Error\n[5] Join Discord\n[6] Exit")
+    print("\n[1] Disable V-Sync Steam\n[2] Disable V-Sync Epic Games\n[3] Disable V-Sync Microsoft Store\n[4] Fix Config Error(Restore to default settings)\n[5] Join Discord\n[6] Exit")
 
 def disableVsync(): # MAIN PROGRAM!!!
 
@@ -53,19 +53,19 @@ def disableVsync(): # MAIN PROGRAM!!!
                 break
 
             else:
-                print(Fore.RED+"Invalid Command! Please enter a number!"+Fore.WHITE)
+                print(Fore.RED+"\nInvalid Command! Please enter a number!"+Fore.WHITE)
                 time.sleep(2)
                 os.system('cls')
                 return disableVsync()
 
         except FileExistsError:
-            print(Fore.RED+"An Unknown Error Occured!"+Fore.WHITE)
+            print(Fore.RED+"\nAn Unknown Error Occured!"+Fore.WHITE)
             time.sleep(2)
             os.system('cls')
             return disableVsync()
 
         try:
-            print(Fore.GREEN+"Disabling your V-Sync..."+Fore.WHITE)
+            print(Fore.GREEN+"\nDisabling your V-Sync..."+Fore.WHITE)
             time.sleep(2)
 
             # Check if Read Only, then set to not Read Only
@@ -74,26 +74,39 @@ def disableVsync(): # MAIN PROGRAM!!!
 
             Engine = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\Engine.ini" # Engine File
             os.chmod(Engine, stat.S_IWRITE)
+            
+            # Remove Current Engine file and replace it with default Engine file & Copy the original to Config Location
+            Engine = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\Engine.ini" # Engine File
+            os.remove(Engine)
 
-            GameUserSettings_Res = "Resources\\GameUserSettings.ini" # Config File
-            os.chmod(GameUserSettings_Res, stat.S_IWRITE)
+            Engine_src = "Resources\\Backup\\Engine.ini"
+            Engine_dst = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\"
+            shutil.copy(Engine_src, Engine_dst)
 
-            Engine_Res = "Resources\\Engine.ini" # Engine File
-            os.chmod(Engine_Res, stat.S_IWRITE)
 
-            # GameUserSettings.ini (Copy from Original Location)
+            try:
+                GameUserSettings_Res = "Resources\\GameUserSettings.ini" # Config File
+                os.chmod(GameUserSettings_Res, stat.S_IWRITE)
+
+                Engine_Res = "Resources\\Engine.ini" # Engine File
+                os.chmod(Engine_Res, stat.S_IWRITE)
+
+            except FileNotFoundError:
+                pass
+
+            # GameUserSettings.ini (Copy from Current Location)
             config_dst = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\GameUserSettings.ini" # Config File
             config_target = "Resources"
 
             shutil.copy(config_dst, config_target) # Copy the original Config to Resources folder, to edit and re-copy it again to dbd config file location
 
-            # Engine.ini (Copy from Original Location)
-            engine_dst = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\Engine.ini"
+            # Engine.ini (Copy from Current Location)
+            engine_dst = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\Engine.ini" # Engine File
             engine_target = "Resources"
 
             shutil.copy(engine_dst, engine_target) # Copy the original Engine to Resources folder, to edit and re-copy it again to dbd config file location
 
-            # Change Config
+            # Change Config (MOST IMPORTANT CODE!!!)
             with open("Resources\\GameUserSettings.ini", "rt") as config_res, open(f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\GameUserSettings.ini", "wt") as config_dbd:
                 for line in config_res:
                     config_dbd.write(line.replace('bUseVSync=True','bUseVSync=False'))
@@ -106,13 +119,13 @@ def disableVsync(): # MAIN PROGRAM!!!
             os.chmod(Engine, S_IREAD|S_IRGRP|S_IROTH) # Lock Engine.ini, prevent the dbd restore this file to default
 
             # Finished
-            print(Fore.GREEN+"V-Sync Disabled!"+Fore.WHITE)
+            print(Fore.GREEN+"\nV-Sync Disabled!"+Fore.WHITE)
             time.sleep(2)
             os.system('cls')
             return disableVsync()
 
         except FileNotFoundError:
-            print(Fore.RED+"Config File Not Found!"+Fore.WHITE)
+            print(Fore.RED+"\nConfig File Not Found!"+Fore.WHITE)
             time.sleep(2)
             os.system('cls')
             continue
@@ -126,7 +139,7 @@ def openDiscord(): # Open my discord server to default browser
 def fixerror(): # Fix Config Error & Restore it to default
     ascii()
     print("Select your Game Platfrom!")
-    print("[1] Restore DBD Steam Config\n[2] Restore DBD Epic Games Config\n[3] Restore DBD Microsoft Config")
+    print("\n[1] Restore DBD Steam Config\n[2] Restore DBD Epic Games Config\n[3] Restore DBD Microsoft Config")
     command = str(input(Fore.GREEN+"\n>> "+Fore.WHITE)) # Input Command
     if command == "1":
         selectedGamePlatfrom = gamePlatfrom[0]
@@ -137,7 +150,7 @@ def fixerror(): # Fix Config Error & Restore it to default
     elif command == "3":
         selectedGamePlatfrom = gamePlatfrom[2]
 
-    print("Restoring your config files...")
+    print("\nRestoring your config files...")
     time.sleep(2)
     try:
         # Remove Read Only to remove the files
@@ -169,12 +182,12 @@ def fixerror(): # Fix Config Error & Restore it to default
         Engine_Dst = f"{home}\\AppData\\Local\\DeadbyDaylight\\Saved\\Config\\{selectedGamePlatfrom}\\Engine.ini"
         shutil.copy(Engine_Backup, Engine_Dst)
 
-        print(Fore.GREEN+"Config has been restored to default settings!"+Fore.WHITE)
+        print(Fore.GREEN+"\nConfig has been restored to default settings!"+Fore.WHITE)
         time.sleep(2)
         os.system('cls')
 
     except FileNotFoundError:
-        print("Config Not Found! Please select valid platfrom!")
+        print("\nConfig Not Found! Please select valid platfrom!")
         os.system('cls')
         time.sleep(2)
         pass
